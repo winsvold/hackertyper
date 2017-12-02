e556545167ed5c70fd18d02af6fd089013d52870
Daniel Winsvold <danielwinsvold@gmail.com>
2017-12-02 23:48:28 +0100

    Access granted and acces denied

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 import {codeGetter} from "./code";
+import Granted from "./Granted";
+import Denied from "./Denied";
 
 class App extends Component {
 
-    handleKeyDown(event){
-        const newSizeOfText = parseInt(this.state.sizeOfText + Math.random()*10);
-        this.setState({
-            sizeOfText: newSizeOfText
-        })
+    handleKeyDown(event) {
+        console.log(event);
+        if (event.key === "Enter") {
+            this.setState({
+                popUp: "granted"
+            })
+        } else if (event.key === "Backspace"){
+            this.setState({
+                popUp: "denied"
+            })
+        } else if (event.key === "Shift"){
+            this.setState({
+                popUp: ""
+            })
+        } else {
+            const newSizeOfText = parseInt(this.state.sizeOfText + Math.random() * 10);
+            this.setState({
+                sizeOfText: newSizeOfText
+            })
+        }
     };
 
     componentWillMount(){
@@ -18,17 +36,38 @@ class App extends Component {
         })
     }
 
+    componentDidUpdate() {
+        this.scrollToBottom();
+    }
+
+    scrollToBottom = () => {
+        const node = ReactDOM.findDOMNode(this.messagesEnd);
+        node.scrollIntoView({ behavior: "smooth" });
+    };
+
     getText(){
         const text = codeGetter(this.state.sizeOfText).split(" ").join("\u00a0").split("\n").map((item, key) => {
-            return <p key={key}>{item}<br/></p>
+            return <p key={key}><br />{item}</p>
         });
         return text;
     };
 
     render() {
+        var popup = '';
+        if(this.state.popUp === "granted"){
+            popup = <Granted />
+        } else if (this.state.popUp === "denied"){
+            popup = <Denied />
+        }
         return (
             <div className="App">
-                {this.getText()}
+                <div className="console">
+                    {this.getText()}
+                    <span className="blink_me">|</span>
+                </div>
+                <div style={{ float:"left", clear: "both" }}
+                     ref={(el) => { this.messagesEnd = el; }}></div>
+                {popup}
             </div>
         );
     }
---
+++ src/Granted.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Granted() {
+    return (
+        <div className="granted">
+            <h2>Acess granted</h2>
+        </div>
+    );
+}
+
+export default Granted;
