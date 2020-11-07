a33b0b8fea57e42cba274ed0abae532ba7705db5
Daniel Winsvold <danielwinsvold@gmail.com>
2020-11-07 16:22:48 +0100

    litt refaktorering, introduserer typescript og setter opp prettier/husky/lint-staged

---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import "./app.less";
+import { hackerCode } from "./resources/codeSample";
+import InitializeMatrix from "./modules/matrix/autoScaleMatrix";
+import { popUpStates } from "./modules/popup/PopUp";
+import PopUp from "./modules/popup/PopUp";
+import ProgressBar from "./modules/progressBar/progressbar";
+import SideMenu from "./modules/sidemenu/SideMenu";
+import { instructions } from "./resources/instructions";
+import SelfWritingConsole from "./modules/selfWritingConsole/SelfWritingConsole";
+import wakeUpTexts from "./modules/selfWritingConsole/text";
+import Console from "./modules/console/console";
+import { isMobile } from "react-device-detect";
+import ReactGA from "react-ga";
+import mobileWarningText from "./resources/mobileWarningText";
+
+type PopUps = "granted" | "denied" | "progressBar" | "wakeUp" | undefined;
+
+function App() {
+  const [sizeOfText, setSizeOfText] = useState(0);
+  const [popUp, setPopUp] = useState<PopUps>();
+  const [loadedPopUp, setLoadedPopUp] = useState<PopUps>();
+  const [matrix, setMatrix] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    ReactGA.initialize("UA-111572952-1");
+    if (isMobile) {
+      ReactGA.pageview("Mobile Warning");
+    } else {
+      ReactGA.pageview("Page View");
+    }
+  }, []);
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === "1") {
+      setLoadedPopUp("granted");
+    } else if (event.key === "2") {
+      setLoadedPopUp("denied");
+    } else if (event.key === "3") {
+      setLoadedPopUp("progressBar");
+    } else if (event.key === "4") {
+      setLoadedPopUp("wakeUp");
+    } else if (event.key === "Control") {
+      ReactGA.event({
+        category: "Features",
+        action: "Toggled Matrix",
+      });
+      setMatrix(!matrix);
+    } else if (event.key === "Enter") {
+      ReactGA.event({
+        category: "Features",
+        action: loadedPopUp || "",
+      });
+      setPopUp(loadedPopUp);
+    } else if (event.key === "Shift") {
+      ReactGA.event({
+        category: "Features",
+        action: "Cleared Feature",
+      });
+      setPopUp(undefined);
+    } else if (event.key === "Delete" || event.key === "Backspace") {
+      const newSizeOfText = Math.round(sizeOfText - Math.random() * 8);
+      setSizeOfText(newSizeOfText >= 0 ? newSizeOfText : 0);
+    } else {
+      setSizeOfText(Math.round(sizeOfText + Math.random() * 8));
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [loadedPopUp, sizeOfText, matrix]);
+
+  return (
+    <div className="App">
+      <Console text={hackerCode} numberOfLetters={sizeOfText} />
+      {matrix && <InitializeMatrix />}
+      <SideMenu content={instructions} open={menuOpen} callback={() => setMenuOpen(!menuOpen)} />
+      {isMobile && <SelfWritingConsole className="mobileWarning" textComponents={mobileWarningText} />}
+      {popUp === "wakeUp" && <SelfWritingConsole textComponents={wakeUpTexts} />}
+      {popUp === "progressBar" && <ProgressBar callBack={() => setPopUp(Math.random() < 0.5 ? "granted" : "denied")} />}
+      {popUp === "denied" && <PopUp popUpState={popUpStates.DENIED} />}
+      {popUp === "granted" && <PopUp popUpState={popUpStates.GRANTED} />}
+    </div>
+  );
+}
+
+export default App;
--- src/modules/selfWritingConsole/text.js
+++ src/modules/selfWritingConsole/text.js
@@ -1,78 +1,96 @@
-export const texts = [{
+export const wakeUpTexts = [
+  {
     delay: 2000,
     clear: true,
-    text: 'Wake up, Neo',
-    interval: 200
-},{
+    text: "Wake up, Neo",
+    interval: 200,
+  },
+  {
     delay: 1000,
     clear: false,
-    text: '...',
-    interval: 500
-},{
+    text: "...",
+    interval: 500,
+  },
+  {
     delay: 3000,
     clear: true,
-    text: 'The Matrix has you.',
-    interval: 300
-},{
+    text: "The Matrix has you.",
+    interval: 300,
+  },
+  {
     delay: 4000,
     clear: true,
-    text: 'Follow the white rabbit.',
-    interval: 150
-},{
+    text: "Follow the white rabbit.",
+    interval: 150,
+  },
+  {
     delay: 4000,
     clear: true,
-    text: 'Knock, knock, Neo.',
-    interval: 50
-},{
+    text: "Knock, knock, Neo.",
+    interval: 50,
+  },
+  {
     delay: 20000,
     clear: true,
-    text: 'Still there',
-    interval: 300
-},{
+    text: "Still there",
+    interval: 300,
+  },
+  {
     delay: 500,
     clear: false,
-    text: '..?',
-    interval: 500
-},{
+    text: "..?",
+    interval: 500,
+  },
+  {
     delay: 5000,
     clear: true,
-    text: 'Peek-a-boo, you are doomed.',
-    interval: 250
-},{
+    text: "Peek-a-boo, you are doomed.",
+    interval: 250,
+  },
+  {
     delay: 3000,
     clear: true,
-    text: '',
-    interval: 250
-},{
+    text: "",
+    interval: 250,
+  },
+  {
     delay: 3000,
     clear: true,
-    text: 'At will I could slay thousands of your best citizens, for I am in close relationship with the Angel of Death.',
-    interval: 170
-},{
+    text:
+      "At will I could slay thousands of your best citizens, for I am in close relationship with the Angel of Death.",
+    interval: 170,
+  },
+  {
     delay: 3000,
     clear: true,
-    text: '',
-    interval: 250
-},{
+    text: "",
+    interval: 250,
+  },
+  {
     delay: 5000,
     clear: true,
-    text: 'Lord, Grant me the serenity to accept the things I cannot change, change the things I can and the wisdom to hide the bodies of those people I had to kill because they pissed me off.',
-    interval: 150
-},{
+    text:
+      "Lord, Grant me the serenity to accept the things I cannot change, change the things I can and the wisdom to hide the bodies of those people I had to kill because they pissed me off.",
+    interval: 150,
+  },
+  {
     delay: 10000,
     clear: true,
-    text: '',
-    interval: 150
-},{
+    text: "",
+    interval: 150,
+  },
+  {
     delay: 10000,
     clear: true,
-    text: 'Got yr letter. What school do you go to? Who’s yr friend? You should stay in school. Send pictures.',
-    interval: 170
-},{
+    text: "Got yr letter. What school do you go to? Who’s yr friend? You should stay in school. Send pictures.",
+    interval: 170,
+  },
+  {
     delay: 10000,
     clear: true,
-    text: '',
-    interval: 250
-}];
+    text: "",
+    interval: 250,
+  },
+];
 
-export default texts;
+export default wakeUpTexts;
