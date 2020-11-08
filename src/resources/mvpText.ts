23bc5e9d076202aa48a728c5c95c347a419ead58
Daniel Winsvold <danielwinsvold@gmail.com>
2020-11-08 10:20:39 +0100

    jobber litt med timing i tekst

--- src/resources/mvpText.ts
+++ src/resources/mvpText.ts
@@ -1,10 +1,10 @@
 import { createTextFragment, TextFragment } from "../modules/selfWritingConsole/textFragment";
 
 export const mvpText: TextFragment[] = [
-  createTextFragment("MVP", 100, 2000),
-  createTextFragment(" - Minimum viable productions", 60, 1500),
-  createTextFragment(" presents", 100, 500),
-  createTextFragment("..", 200, 500),
+  createTextFragment("MVP", 100, 2500),
+  createTextFragment(" - Minimum Viable Productions", 60, 1000),
+  createTextFragment(" presents", 100, 300),
+  createTextFragment("..", 200, 300),
 ];
 
 export default mvpText;
